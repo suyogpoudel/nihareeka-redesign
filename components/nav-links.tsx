2e4bb7925f63ad6a533f3f23d222899ef4d6aa00
Suyog Poudel <suyogpoudel583@gmail.com>
2026-02-10 17:13:28 +0545

    removed scrolling on menu open

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Separator } from "./ui/separator";
 import RegisterButton from "./register-button";
 import { usePathname } from "next/navigation";
@@ -29,6 +29,14 @@ const links = [
 const NavLinks = () => {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "unset";
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
 
   const renderLinks = () =>
     links.map((link) => (
